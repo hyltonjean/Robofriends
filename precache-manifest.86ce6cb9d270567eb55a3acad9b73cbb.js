05b491c817b3b5dbd75ad4a33943d52978e6e8df
hyltonjean <hyltonjean@gmail.com>
2018-11-01 11:49:43 +0200

    Updates

---
+++ precache-manifest.86ce6cb9d270567eb55a3acad9b73cbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39a3a35c71e0df973673",
+    "url": "/Robofriends/static/js/runtime~main.39a3a35c.js"
+  },
+  {
+    "revision": "31dde5bf2074218d7e1a",
+    "url": "/Robofriends/static/js/main.31dde5bf.chunk.js"
+  },
+  {
+    "revision": "ca2ae2037b60fb533601",
+    "url": "/Robofriends/static/js/1.ca2ae203.chunk.js"
+  },
+  {
+    "revision": "31dde5bf2074218d7e1a",
+    "url": "/Robofriends/static/css/main.571ebe81.chunk.css"
+  },
+  {
+    "revision": "ca2ae2037b60fb533601",
+    "url": "/Robofriends/static/css/1.176f961d.chunk.css"
+  },
+  {
+    "revision": "6721139066b31e6e4e6b8a19b74520c5",
+    "url": "/Robofriends/index.html"
+  }
+];
